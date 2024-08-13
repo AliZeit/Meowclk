ed0c953d94244d12c1d4388c32548dc43a5683f3
AliZeit <156523570+AliZeit@users.noreply.github.com>
2024-08-13 12:09:10 +0400

    new

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-let catCount = 0
-let clickPower =1
-
+// main vars
 let btn = document.querySelector('#cat')
 let span = document.querySelector('#main-number')
+
+let upgrades = document.querySelectorAll(`.upgrade`)
+
+// main number vars
+let catCount = 0
+let clickPower = 1
+
+// costs
+let costs = [10 , 50 , 400, 3000]
+
+
+for (let button of buttons){
+    button.addEventListener()
+}
 
 btn.addEventListener('click', add)
 
